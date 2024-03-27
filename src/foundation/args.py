07194ac6b57a0d5b6d2082dd5323811d025d8b14
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2024-03-27 19:23:37 -0400

    change train_data_dir for testing run code

--- src/foundation/args.py
+++ src/foundation/args.py
@@ -15,7 +15,7 @@ root_dir = '/NFS/Users/moonsh/FM_biosignal'
 Modalities = 'hr', 'ecg', 'activity'
 """
 
-base_config = {'train_data_dir': '/NFS/Users/moonsh/data/mesa/preproc/pair',
+base_config = {'train_data_dir': '/NFS/Users/moonsh/data/mesa/preproc/pair_small', # 'pair' is real train data
                'val_data_dir': '/NFS/Users/moonsh/data/mesa/preproc/pair',
                'test_data_dir': '/NFS/Users/moonsh/data/mesa/preproc/pair_test',
                'modalities': ['ecg', 'hr'],
