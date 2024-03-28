7d6a911676eadfffc4e2886b32e792f484b20214
SXH <victo7393@gmail.com>
2024-03-27 21:13:43 -0400

    calculate acc and loss

---
+++ src/foundation/train_utils/metric.py
@@ -0,0 +1,14 @@
+import torch
+
+
+
+def get_acc_loss_from_dataloader(dataloader):
+    
+    for in data_loader
+
+    
+    return acc, loss
+
+def get_accuracy_from_train_process(logit_arr, true_label):
+    
+    return acc
