25a4353c68f42b338d741a9e4ed708b59dbc9c40
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2024-03-29 18:10:13 -0400

    add function for numpy data type

--- preproc/featureExtractor.py
+++ preproc/featureExtractor.py
@@ -123,3 +123,61 @@ def subject_extractor(data_dir):
         result.append(subject_id)
         
     return result
+
+
+def get_ecg_features_np(subject, load_dir, save_dir):
+    
+    test_ecg = np.load(os.path.join(load_dir, f'{subject}_ecg.npy'))
+    test_ecg = test_ecg.squeeze().reshape(-1)
+    
+    fs = 256
+
+    ecg_proc, info = my_processing(test_ecg, fs)
+    
+    #epoch data  
+    window = 30 
+    intervals = np.arange(0, len(ecg_proc)-1, fs*window)
+    epochs = nk.epochs_create(ecg_proc, events=intervals, sampling_rate=fs)
+
+    epochs_df = nk.epochs_to_df(epochs)
+    epochs_df = epochs_df[0:len(ecg_proc)] #fix issue with output size
+
+    feat = nk.ecg_intervalrelated(epochs)
+
+    #fix issues with output being in list
+
+    columns_to_unbracket = feat.columns[2:]
+
+    for col in columns_to_unbracket:
+        feat[col] = feat[col].apply(lambda x: x[0][0])
+
+    #labels 
+    labels = np.load(os.path.join(load_dir, f'{subject}_labeled_sleep.npy'))
+    labels = pd.DataFrame(labels, columns=['psg_status'])
+    epochs_df['labels'] = labels['psg_status']
+
+    # binary classification
+    epochs_df['labels2'] = labels['psg_status'].replace([1, 2, 3, 4, 5], 1)
+
+    # 4 classes
+    epochs_df['labels4'] = labels['psg_status'].replace([1,2], 1)
+    epochs_df['labels4'] = epochs_df['labels4'].replace([3,4], 2)
+    epochs_df['labels4'] = epochs_df['labels4'].replace([5], 3)
+    
+    #same for features 
+    labels_trunc = labels.iloc[::256*30, :].reset_index()
+
+
+    feat['labels'] = labels_trunc['psg_status'].values
+
+    feat['labels2'] = feat['labels'].replace([1, 2, 3, 4, 5], 1).values
+
+    feat['labels4'] = feat['labels'].replace([1,2], 1).values
+    feat['labels4'] = feat['labels4'].replace([3,4], 2).values
+    feat['labels4'] = feat['labels4'].replace([5], 3).values
+
+    # print(feat.head())
+    # print(epochs_df.head())
+    
+    feat.to_csv(os.path.join(save_dir,'subject_'+subject+'_ecg_feat.csv'))
+    epochs_df.to_csv(os.path.join(save_dir,'subject_'+subject+'_ecg_proc.csv'))
