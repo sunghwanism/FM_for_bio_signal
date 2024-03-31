6e3027f5995ec5ed996928334174fd24522b77ec
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2024-03-31 02:32:49 -0400

    del weight_loss in Adversarialmodel.py

--- src/foundation/models/AdversarialModel.py
+++ src/foundation/models/AdversarialModel.py
@@ -50,17 +50,16 @@ class AdversarialModel(nn.Module):
             adversarial_loss += -1. * torch.log(log_noise + subject_outs[i, j])
         return adversarial_loss
 
-    def forward_subject_invariance_loss(self, subject_preds, subject_labels, adversarial_weighting_factor):
+    def forward_subject_invariance_loss(self, subject_preds, subject_labels):
         subject_labels = F.one_hot(subject_labels, num_classes=self.num_subjects)
         subject_outs = F.normalize(subject_preds, p=2, dim=1)
 
         BATCH_DIM = 0
         log_noise = 1e-12
         subject_invariance_loss = 0.
-        weight_factor = adversarial_weighting_factor
         curr_batch_size = subject_outs.size(BATCH_DIM)
 
         for i in range(curr_batch_size):
             j = torch.argmax(subject_labels[i, :])
-            subject_invariance_loss += weight_factor * (-1.) * torch.log(log_noise + (1. - subject_outs[i, j]))
+            subject_invariance_loss += (-1.) * torch.log(log_noise + (1. - subject_outs[i, j]))
         return subject_invariance_loss
