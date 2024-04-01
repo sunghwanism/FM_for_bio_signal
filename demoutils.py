64127fafb3f257d57a54d243a83237423e5eafa3
sunghwanism <97149338+sunghwanism@users.noreply.github.com>
2024-04-01 07:01:33 -0400

    update demo

--- demoutils.py
+++ demoutils.py
@@ -1,16 +1,35 @@
 import os
+import sys
+sys.path.append("./src")
+sys.path.append("./src/foundation")
+sys.path.append("./basemodel")
 
 import torch
 from torch.utils.data import Dataset
 import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+
+from foundation.data.Augmentaion import init_augmenter
+from foundation.models.FOCALModules import FOCAL
+from foundation.models.Backbone import DeepSense
+from basemodel.DeepSense import DeepSense as BASEDEEPSENSE
+from downstream.classifier import SleepStageClassifier
+import foundation.args as args
+import downstream.downargs as downargs
+import baselineargs
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+
+device = torch.device('mps')
+
+# aug_1 = init_augmenter("NoAugmenter", None).to(device)
+# aug_2 = init_augmenter("NoAugmenter", None).to(device)
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'mps')
-
-
-class MESADataset(Dataset):
+class MESAPairDataset(Dataset):
     def __init__(self, file_path, modalities=['ecg', 'hr'], subject_idx='subject_idx', stage='stage'):
-        super(MESADataset, self).__init__()
+        super(MESAPairDataset, self).__init__()
         self.root_dir = file_path
         self.files = os.listdir(file_path)
         self.modalities = modalities
@@ -45,15 +64,15 @@ class MESADataset(Dataset):
         sample = [self.modality_1, self.modality_2, self.subject_id, self.sleep_stage]
         
         return sample
+    
+    
 
-
-def get_predict_lable_from_dataloader(model, dataloder, device, criterion):
+def get_predict_label_from_fm_classifier(model, downstream_model, dataloder, device):
     
     model.eval()
     
-    total_correct = 0
-    total_samples = 0
-    total_loss = 0
+    pred_list = []
+    labels = []
     
     for i, data in enumerate(dataloder):
         ecg, hr, _, sleep_stage = data
@@ -61,37 +80,170 @@ def get_predict_lable_from_dataloader(model, dataloder, device, criterion):
         hr = hr.to(device)
         sleep_stage = sleep_stage.to(device)
         
-        output = model(ecg, hr, class_head=True, proj_head=True)
-        loss = criterion(output, sleep_stage)
+        aug_1_modal_1 = aug_1(ecg)
+        aug_2_modal_1 = aug_2(ecg)
         
-        total_loss += loss.item()
-        total_correct += torch.sum(torch.argmax(output, dim=1) == sleep_stage).item()
-        total_samples += sleep_stage.size(0)
+        aug_1_modal_2 = aug_1(hr)
+        aug_2_modal_2 = aug_2(hr)
         
-    return total_correct / total_samples, total_loss / (i+1)
+        mod_feature1, mod_feature2 = model(aug_1_modal_1, aug_1_modal_2, 
+                                           aug_2_modal_1, aug_2_modal_2, proj_head=True, class_head=False)
+        
+        preds = downstream_model(mod_feature1, mod_feature2)
+        pred_list.extend(preds.detch().cpu().numpy().squeeze())
+        labels.extend(sleep_stage.detch().cpu().numpy().squeeze())
+        
+    return pred_list, labels
 
+
+
+
+def get_predict_label_from_individual_model(individual_model, dataloder, device):
+    
+    individual_model.eval()
+    
+    preds = []
+    labels = []
+    
+    for i, data in enumerate(dataloder):
+        ecg, hr, _, sleep_stage = data
+        
+        ecg = ecg.to(device)
+        hr = hr.to(device)
+        sleep_stage = sleep_stage.to(device)
+
+        pred = individual_model(ecg, hr, class_head=True, proj_head=True)
+        
+        pred = torch.argmax(pred, dim=1)
+        
+        preds.extend(pred.detach().cpu().numpy().squeeze())
+        labels.extend(sleep_stage.detach().cpu().numpy().squeeze())
+        
+        
+    return preds, labels
 
 
 def Load_Foundation_Model(model_ckpt):
     pass
 
 
-def predict_using_individual_model_mesa(subj_index):
-    
+def predict_using_individual_model(subj_index):
     
     subj_test_data_path = f'pair_test_subj/subj_{subj_index}_test'
+    subj_test_model_ckpt = f'models/individual_best/DeepSense_{subj_index}.pth'
     
-    # model은 deepsense
+    ckpt = torch.load(subj_test_model_ckpt, map_location=device)
+    
+    baselineargs.data_config = ckpt['data_config']
+    baselineargs.trainer_config = ckpt['trainer_config']
+    model_param = ckpt['state_dict']
+    
+    individual_model = BASEDEEPSENSE(baselineargs).to(device)
+    individual_model.load_state_dict(model_param)
+
+    test_dataset = MESAPairDataset(file_path=subj_test_data_path,
+                                modalities=baselineargs.data_config['modalities'],
+                                subject_idx=baselineargs.data_config['subject_key'],
+                                stage=baselineargs.data_config['label_key'])
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                                batch_size=128,
+                                                shuffle=False,
+                                                num_workers=4)
+    
+    predict, labels = get_predict_label_from_individual_model(individual_model, test_loader, device)
+    
+    # Calculate metrics
+    acc = accuracy_score(labels, predict)
+    f1score = f1_score(labels, predict, average='macro')
+    conf_matrix = confusion_matrix(labels, predict)
+
+    print(f'Accuracy: {round(acc, 3)}')
+    print(f'F1-score (Macro): {round(f1score,3)}')
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+    
+    return acc, f1score
+
+def predict_using_fm_classifier(subj_index, model_index):
+    
+    subj_test_data_path = f'pair_test_subj/subj_{subj_index}_test'
+    if subj_index == '0560':
+        subj_test_model_ckpt = f'models/ckpt_down/{subj_index}/FM_based_classfier_0140.pth'
+    elif subj_index == "0565":
+        subj_test_model_ckpt = f'models/ckpt_down/{subj_index}/FM_based_classfier_0143.pth'
+    elif subj_index == "0583":
+        subj_test_model_ckpt = f'models/ckpt_down/{subj_index}/FM_based_classfier_0143.pth'
+    elif subj_index == "0558":
+        subj_test_model_ckpt = f'models/ckpt_down/{subj_index}/FM_based_classfier_0143.pth'
+    elif subj_index == "0571":
+        subj_test_model_ckpt = f'models/ckpt_down/{subj_index}/FM_based_classfier_0143.pth'
+    else:
+        ValueError("Invalid subject index")
+    
+    model_config = torch.load(subj_test_model_ckpt, map_location=device)
+    
+    args['data_config'] = model_config['focal_data_config']
+    args['trainer_config'] = model_config['focal_trainer_config']
+    args['focal_config'] = model_config['focal_config']
+    
+    downargs['downstream_config'] = model_config['downstream_config']
     
     
-    predict, labels = get_predict_lable_from_dataloader(model, dataloder, device, criterion)
+    backbone = DeepSense(args).to(device)
+    focal_model = FOCAL(args, backbone).to(device)
+    focal_model.load_state_dict(model_config["focal_state_dict"], strict=False)
+    
+    downstream_model = SleepStageClassifier(args).to(device)
+    downstream_model.load_state_dict(model_config["down_state_dict"], strict=False)
+    
+
+    test_dataset = MESAPairDataset(file_path=subj_test_data_path,
+                                modalities=args.data_config['modalities'],
+                                subject_idx=args.data_config['subject_key'],
+                                stage=args.data_config['label_key'])
+
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                                batch_size=args.trainer_config['batch_size']//4,
+                                                shuffle=False,
+                                                num_workers=2)
+    
+    predict, labels = get_predict_label_from_fm_classifier(model, downstream_model, test_loader, device)
+    
+    # Calculate metrics
+    acc = accuracy_score(labels, predict)
+    f1score = f1_score(labels, predict, average='macro')
+    conf_matrix = confusion_matrix(labels, predict)
+
+    print(f'Accuracy: {round(acc, 3)}')
+    print(f'F1-score (Macro): {round(f1score,3)}')
+    plt.figure(figsize=(5, 4))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
     
     
-    # Accuracy
-    # F1-score macro
-    # confusion matrix
+def loss_acc_plotting(subj_index, model_index='0140'):
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     
     
-    print(acc)
-    print(f1score)
+    ax[0].plot(np.load(f"./logs/logs_down/{subj_index}/FM_based_classfier_{model_index}.npz")["arr_0"][0], label='train')
+    ax[0].plot(np.load(f"./logs/logs_down/{subj_index}/FM_based_classfier_{model_index}.npz")["arr_0"][2], label='val')
     
+    ax[0].set_xlabel('Epoch')
+    ax[0].set_ylabel('Loss')
+    ax[0].legend()
+    
+    ax[1].plot(np.load(f"./logs/logs_down/{subj_index}/FM_based_classfier_{model_index}.npz")["arr_0"][1], label='train')
+    ax[1].plot(np.load(f"./logs/logs_down/{subj_index}/FM_based_classfier_{model_index}.npz")["arr_0"][3], label='val')
+    
+    ax[1].set_xlabel('Epoch')
+    ax[1].set_ylabel('Accuracy')
+    ax[1].legend()
+    
+    plt.show()
